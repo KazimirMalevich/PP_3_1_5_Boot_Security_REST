75bb5f0e97872e668de6e7d8e997691f5e2de344
Dmitrii Obukhov <dmitriyforyou@icloud.com>
2023-10-23 16:44:33 +0300

    Commit 2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MyRESTController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MyRESTController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
@@ -19,8 +22,8 @@ import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import java.util.Set;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/")
 public class MyRESTController {
     private final UserService userService;
     private final RoleService roleService;
@@ -30,19 +33,48 @@ public class MyRESTController {
         this.roleService = roleService;
     }
 
-    @GetMapping("/auth")
+    @GetMapping(value = "admin")
+    public String allUsers(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+        User user = new User();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User authUser = userService.getUserByUsername(auth.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("roless", roleService.allRoles());
+        model.addAttribute("authUser", authUser);
+        return "adminPage";
+    }
+
+    @GetMapping(value = "login")
+    public String getLoginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping(value = "user")
+    public String enterUser(ModelMap model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User authUser = userService.getUserByUsername(auth.getName());
+        model.addAttribute("authUser", authUser);
+        System.out.println(authUser);
+        return "userPage2";
+    }
+
+    @ResponseBody
+    @GetMapping("api/auth")
     public User getAythUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User authUser = userService.getUserByUsername(auth.getName());
         return authUser;
     }
 
-    @GetMapping("/users")
+    @ResponseBody
+    @GetMapping("api/users")
     public Set<User> showAllUsers() {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/users/{id}")
+    @ResponseBody
+    @GetMapping("api/users/{id}")
     public User getUserFromDB(@PathVariable Integer id) {
         User user = userService.getUser(id);
         if (user==null) {
@@ -51,24 +83,29 @@ public class MyRESTController {
         return user;
     }
 
-    @GetMapping("/users/roles")
+    @ResponseBody
+    @GetMapping("api/users/roles")
     public ResponseEntity<Set<Role>> getAllRoles(){
         Set <Role> roleList = roleService.allRoles();
         return new ResponseEntity<>(roleList,HttpStatus.OK);
     }
 
-    @PostMapping("/users")
+    @ResponseBody
+    @PostMapping("api/users")
     public ResponseEntity<HttpStatus> addNewUser(@RequestBody User user) {
        userService.saveUser(user);
        return ResponseEntity.ok(HttpStatus.OK);
     }
-    @PatchMapping("/users")
+
+    @ResponseBody
+    @PatchMapping("api/users")
     public ResponseEntity<HttpStatus> updateUser(@RequestBody User user) {
         userService.update(user);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @DeleteMapping("/users/{id}")
+    @ResponseBody
+    @DeleteMapping("api/users/{id}")
     public String deleteUser(@PathVariable Integer id) {
         User user = userService.getUser(id);
         if (user == null) {
